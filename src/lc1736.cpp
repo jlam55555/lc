c50c5f7627b6a9c26100d591c597010ec49a5f08
Jonathan Lam <jlam55555@gmail.com>
2023-02-06 12:23:17 -0800

    Add lc1736.cpp

---
+++ src/lc1736.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  string maximumTime(string time) {
+    // Handle hours.
+    if (time[0] == '?' && time[1] == '?') {
+      time[0] = '2';
+      time[1] = '3';
+    } else if (time[0] == '?') {
+      time[0] = time[1] < '4' ? '2' : '1';
+    } else if (time[1] == '?') {
+      time[1] = time[0] == '2' ? '3' : '9';
+    }
+
+    // Handle minutes.
+    if (time[3] == '?') {
+      time[3] = '5';
+    }
+    if (time[4] == '?') {
+      time[4] = '9';
+    }
+    return time;
+  }
+};
