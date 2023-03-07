b6ea00a9e8b265ff60f089e426df7babdc01df6b
Jonathan Lam <jlam55555@gmail.com>
2023-03-06 17:33:15 -0800

    Update lc0639.cpp

--- src/lc0639.cpp
+++ src/lc0639.cpp
@@ -1,54 +1,45 @@
-#define MAX 1000000007
+#define MOD 1000000007
 
 class Solution {
 public:
-  int numDecodings(::string s) {
-    int prev, curr, next, i;
+  // This is very similar to lc0046.cpp (Jump Game II),
+  // with the addition of wildcards. We only need to store
+  // the number of ways to decode the message up to the last
+  // two characters to determine the number of ways to
+  // decode the next character.
+  int numDecodings(string s) {
+    // `prev1` is the number of ways to decode up to the
+    // previous character, `prev2` is the same for the
+    // second most recent character. Similarly, `c0` represents
+    // the current character and `c1` is the previous character.
+    long prev1{1}, prev2{1};
+    char c0, c1;
 
-    for (prev = 1, curr = 0, next = 0, i = 0; i < s.length(); ++i) {
-      // if current character is *
-      if (s[i] == '*') {
-        // multiply current position by 9
-        curr = (curr + prev * 9) % MAX;
+    for (int i{}, n(s.length()); i < n; ++i) {
+      // Count the number of ways this can form a one-digit number.
+      c0 = s[i];
+      auto one_digit_options{c0 == '0' ? 0 : (c0 == '*' ? 9 : 1)};
 
-        // if two letters
-        if (i < s.length() - 1) {
-          // if next letter is known
-          if (s[i + 1] != '*') {
-            // pretend like it's 1
-            next = (next + prev) % MAX;
-
-            // pretend like it's two
-            if (s[i + 1] <= '6')
-              next = (next + prev) % MAX;
-          }
-          // if two unknown combinations, add current * 15
-          // 11-19, 21-26 includes 15 numbers
-          else
-            next = (next + prev * 15) % MAX;
-        }
-      } else {
-        // if single digit number
-        if (s[i] != '0')
-          curr = (curr + prev) % MAX;
-
-        // if two digit number
-        if (i < s.length() - 1 &&
-            (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')) {
-          // if next digit is not asterisk, treat normally
-          if (s[i + 1] != '*')
-            next = (next + prev) % MAX;
-          // if next digit is asterisk, multiply by appropriate number
-          else
-            next = (next + prev * (s[i] == '1' ? 9 : 6)) % MAX;
+      // Count the number of ways this can form a two-digit number.
+      int two_digit_options{};
+      if (i) {
+        if (c0 == '*' && c1 == '*') {
+          two_digit_options = 15;
+        } else if (c0 == '*') {
+          two_digit_options = c1 == '2' ? 6 : (c1 == '1' ? 9 : 0);
+        } else if (c1 == '*') {
+          two_digit_options = c0 <= '6' ? 2 : 1;
+        } else {
+          two_digit_options = (c1 == '1') || (c1 == '2' && c0 <= '6');
         }
       }
 
-      prev = curr;
-      curr = next;
-      next = 0;
+      // Iterate.
+      std::tie(prev1, prev2) = std::tuple(
+          (one_digit_options * prev1 + two_digit_options * prev2) % MOD, prev1);
+      c1 = c0;
     }
 
-    return prev;
+    return prev1 % MOD;
   }
 };
