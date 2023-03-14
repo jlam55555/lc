af9bb71466afabbf2e03fd7602b583a19a985873
Jonathan Lam <jlam55555@gmail.com>
2023-03-13 20:13:32 -0700

    Simplify lc0300.cpp (patience sort for longest monotonic subsequence)

--- src/lc0300.cpp
+++ src/lc0300.cpp
@@ -1,31 +1,16 @@
+// Patience sort for longest increasing subsequence.
 class Solution {
 public:
-  // patience sort for longest increasing subsequence
   int lengthOfLIS(vector<int> &nums) {
-    vector<int> dp{};
-    int piles = 0;
-
-    for (auto &i : nums) {
-
-      // standard binary search to find first position
-      // greater than or equal to i
-      int l = 0, r = piles, m;
-      while (l < r) {
-        m = (l + r) / 2;
-        if (dp[m] < i) {
-          l = m + 1;
-        } else {
-          r = m;
-        }
+    std::vector<int> piles;
+    for (auto i : nums) {
+      if (auto it{std::lower_bound(piles.begin(), piles.end(), i)};
+          it != piles.end()) {
+        *it = i;
+      } else {
+        piles.push_back(i);
       }
-
-      if (l == piles) {
-        ++piles;
-        dp.push_back(0);
-      }
-      dp[l] = i;
     }
-
-    return piles;
+    return piles.size();
   }
 };
